952870a2e8150b2e9342d842ec6c4caf2567dda6
Aditya <rishkat1249@gmail.com>
2026-04-17 05:32:40 +0000

    Making a white dot appear at the center of the display

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
+void set_pixel(Uint32 *pixels, int x , int y , Uint32 color)
+{
+	
+	int index = y * 800 + x ;
+	pixels[index] = color;
+	
+}
+
 int main()
 {
 	 SDL_Init(SDL_INIT_VIDEO);
+	 Uint32 pixels[480000] = {0};
 
 	SDL_Window * window = SDL_CreateWindow("Software rasteriser", SDL_WINDOWPOS_CENTERED , SDL_WINDOWPOS_CENTERED , 800 , 600 , 0 );
 
@@ -12,6 +21,15 @@ int main()
 
 	while(running)
 	{
+
+	set_pixel(pixels, 400, 300, 0xFFFFFFFF);
+	
+	SDL_Surface* surface = SDL_CreateRGBSurfaceFrom(pixels, 800, 600, 32, 3200, 0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF);
+	SDL_Surface* window_surface = SDL_GetWindowSurface(window);
+	SDL_BlitSurface(surface, NULL, window_surface, NULL);
+	SDL_UpdateWindowSurface(window);
+
+	
 		while (SDL_PollEvent(&event))
 		{
 			if(event.type == SDL_QUIT)
@@ -26,3 +44,5 @@ int main()
 
 	return 0;
 }
+
+
